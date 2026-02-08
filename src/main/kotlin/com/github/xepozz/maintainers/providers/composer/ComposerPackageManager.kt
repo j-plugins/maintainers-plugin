06629f6a86bdfb8cdad4a2350cf4fb031f6d2a15
Dmitriy Derepko <xepozz@list.ru>
2026-02-08 14:26:05 +0400

    feat: rename package manager

--- src/main/kotlin/com/github/xepozz/maintainers/providers/composer/ComposerPackageManager.kt
+++ src/main/kotlin/com/github/xepozz/maintainers/providers/composer/ComposerPackageManager.kt
@@ -2,10 +2,9 @@ package com.github.xepozz.maintainers.providers.composer
 
 import com.github.xepozz.maintainers.MaintainersIcons
 import com.github.xepozz.maintainers.model.PackageManager
-import com.intellij.icons.AllIcons
 import javax.swing.Icon
 
 object ComposerPackageManager : PackageManager {
-    override val name: String = "composer"
+    override val name: String = "Composer"
     override val icon: Icon = MaintainersIcons.COMPOSER
 }
--- src/main/kotlin/com/github/xepozz/maintainers/providers/npm/NpmPackageManager.kt
+++ src/main/kotlin/com/github/xepozz/maintainers/providers/npm/NpmPackageManager.kt
@@ -2,10 +2,9 @@ package com.github.xepozz.maintainers.providers.npm
 
 import com.github.xepozz.maintainers.MaintainersIcons
 import com.github.xepozz.maintainers.model.PackageManager
-import com.intellij.icons.AllIcons
 import javax.swing.Icon
 
 object NpmPackageManager : PackageManager {
-    override val name: String = "npm"
+    override val name: String = "NPM"
     override val icon: Icon = MaintainersIcons.NPM
 }
